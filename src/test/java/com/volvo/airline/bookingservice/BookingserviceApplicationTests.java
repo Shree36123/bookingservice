c07e6431c574f9e5c571bb29c225d64c8b42b55a
a460113 <shreeranganatha.nr@consultant.volvo.com>
2024-12-19 21:34:25 +0530

    intial commit

---
+++ src/test/java/com/volvo/airline/bookingservice/BookingserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.volvo.airline.bookingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
